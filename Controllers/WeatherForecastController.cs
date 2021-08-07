2bc9ce221b99f170ecfbfe89cabbb6dcf1df5d63
harinibabu <har.brhv@gmail.com>
2021-08-08 00:44:24 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace weatherforecast.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        public readonly IDynamoDBContext _dynamodbcontext;
+
+        public WeatherForecastController( IDynamoDBContext context,
+            ILogger<WeatherForecastController> logger)
+        {
+            _dynamodbcontext = context;
+            _logger = logger;
+        }
+
+       
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get(string city = "Bangalore")
+        {
+             return await _dynamodbcontext.QueryAsync<WeatherForecast>(city).GetRemainingAsync();
+         //   return GenerateDummmyForecast(city);
+        }
+
+        [HttpPost]
+        public async void Post(string city)
+        {
+            //var data = GenerateDummmyForecast(city);
+            //foreach(var item in data)
+            //{
+            //    await _dynamodbcontext.SaveAsync(item);
+            //}
+
+            var item = await _dynamodbcontext.LoadAsync<WeatherForecast>(city, DateTime.Now.Date.AddDays(1));
+            item.Summary = "Test";
+           await _dynamodbcontext.SaveAsync(item);           
+        }
+
+        [HttpDelete]
+        public  async void Delete(string city)
+        {
+            var item = await _dynamodbcontext.LoadAsync<WeatherForecast>(city, DateTime.Now.Date.AddDays(1));
+            await _dynamodbcontext.DeleteAsync(item);
+        }
+
+        private static IEnumerable<WeatherForecast> GenerateDummmyForecast(string city)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                City = city,
+                Date = DateTime.Now.Date.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
+
+namespace weatherforecast
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "weatherforecast", Version = "v1" });
+            });
+
+            //DynamoDB
+            var credentails = new BasicAWSCredentials("", "");
+            var config = new AmazonDynamoDBConfig()
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.APSouth1
+            };
+
+
+            var client = new AmazonDynamoDBClient(credentails, config);
+            services.AddSingleton<IAmazonDynamoDB>(client);
+            services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "weatherforecast v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
